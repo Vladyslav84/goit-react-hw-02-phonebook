32b8588ae3a0f53f6b30b7595f8e8dd367713661
Vladyslav84 <72793544+Vladyslav84@users.noreply.github.com>
2021-07-08 22:39:09 +0300

    LocalStorage is done.

--- src/App.js
+++ src/App.js
@@ -5,19 +5,41 @@ import ContactList from './components/ContactList/ContactList';
 import s from './App.module.css';
 import { v4 as uuidv4 } from 'uuid';
 
-const contactsArr = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-]
+// const contactsArr = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ]
+
+console.log(null === true);
 
 class App extends Component {
 
   state = {
-    contacts: contactsArr,
+    contacts: [],
     filtered: ''
   }
+
+  componentDidUpdate(prevState) {
+
+    if (this.state.contacts !== prevState.contacts)
+    {
+      localStorage.setItem('contactsList', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  componentDidMount() {
+
+    const parcedContactsList = JSON.parse(localStorage.getItem('contactsList'));
+
+    if (parcedContactsList)
+    {
+      this.setState({ contacts: parcedContactsList })
+    }
+
+  }
+
 
   formSubmithandler = (formData) => {
 
